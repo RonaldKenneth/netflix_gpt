c79576665c16ba2c38f1f9fc6c74af9faaef7483
RonaldKenneth <rronaldkenneth@gmail.com>
2024-04-30 17:30:52 +0530

    openai api integrated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAfcRP8vgkuIbYnOzujarPQ7OlMCvOEYLQ",
-  authDomain: "tmdb-gpt.firebaseapp.com",
-  projectId: "tmdb-gpt",
-  storageBucket: "tmdb-gpt.appspot.com",
-  messagingSenderId: "769578242979",
-  appId: "1:769578242979:web:111102f10ac13811d8c016",
-  measurementId: "G-DK5EGNM0TL"
+ 
 };
 
 // Initialize Firebase
